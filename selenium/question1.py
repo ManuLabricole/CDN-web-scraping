5fb019c9ea893bbe1dc59761c2d02f8dba5df1ef
Manu Labricole <manu_barriol@hotmail.fr>
2023-05-31 09:49:07 +0200

    start section 5

--- selenium/question1.py
+++ selenium/question1.py
@@ -37,6 +37,9 @@ driver = webdriver.Chrome(options=options)
 driver.get('https://pagination.js.org/')
 # driver.maximize_window()
 
+# --------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------
+
 
 # STEP I - CLICK ON THE NEXT BUTTON
 # We want to click on the next button which contains ">" in the html code
@@ -45,19 +48,61 @@ def click_next():
     next_btn.click()
 
 
+# WORK ON THE FIRST TABLE - GET THE NUMBER OF PAGES
 all_list_elements = driver.find_elements(
     By.XPATH, '//*[@id="demo1"]/div[2]/div/ul/li')
+
 print_sep()
 print(
-    f"There are --> {len(all_list_elements)} <-- elements in [@id='demo1']/div[2]/div/ul/li")
+    f"There are --> {len(all_list_elements)} <-- elements in [@id='demo1']/div[2]/div/ul/li"
+)
+
 # Then we want to find the number of pages, so in the html code we will target the second last element of the list
 # (last is next button, second to last is the last page)
 number_of_pages = int(all_list_elements[-2].text)
 print_sep()
 print(f"There are --> {number_of_pages} <-- pages")
 
-for _ in range(number_of_pages):
-    click_next()
-    time.sleep(1)
+# --------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------
+
+# No we want to catch all the elements in the ul list for each page
+# The pagination is a ul, and each li give access to another ul where we want to catch the data
+
+
+def print_all_elements(num):
+
+    # Locate all elements on the current page of the pagination element
+    page_elements = driver.find_elements(
+        By.XPATH, '//*[@id="demo1"]/div[1]/ul/li')
     print_sep()
-    print("NEXT PAGE")
+    print(f"Page {num+1}")
+    for li in page_elements:
+        text_content = li.get_attribute('innerHTML')
+
+        # Print the text content to the console
+        print("")
+        print("--------------------------------------------")
+        print(text_content)
+        print("--------------------------------------------")
+
+
+def loop_through_pages():
+
+    for i in range(number_of_pages):
+        click_next()
+        time.sleep(0.25)
+        print_sep()
+        print("NEXT PAGE")
+        print_all_elements(i)
+
+    print("")
+    print(" -------------------------------- END OF LOOP -------------------------------- ")
+
+
+# RUN THE LOOP THROUGH PAGES FUNCTION AND PRINT THE CONTENT OF THE FIRST ELEMENT
+loop_through_pages()
+
+
+# --------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------
